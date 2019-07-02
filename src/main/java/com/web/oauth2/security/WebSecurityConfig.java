47785d6101fd9b6f2c1c0048b66581c2887ee309
Kazuyuki DEWA <Kazuyuki.Dewa@so-net.co.jp>
2019-07-02 19:43:50 +0900

    ソーシャルログイン機能を除去

--- src/main/java/com/web/oauth2/security/WebSecurityConfig.java
+++ src/main/java/com/web/oauth2/security/WebSecurityConfig.java
@@ -5,10 +5,7 @@ import java.util.List;
 
 import javax.servlet.Filter;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerTokenServicesConfiguration;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,9 +13,6 @@ import org.springframework.context.annotation.Import;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.client.OAuth2ClientContext;
-import org.springframework.security.oauth2.client.OAuth2RestTemplate;
-import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
@@ -39,9 +33,6 @@ import org.springframework.web.filter.CompositeFilter;
 /* Set "ResourceServerConfiguration" class filter preference over this one */
 @Order(6)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-	@Autowired
-	OAuth2ClientContext oauth2ClientContext;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -83,25 +74,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	private Filter ssoFilter() {
 		CompositeFilter filter = new CompositeFilter();
 		List<Filter> filters = new ArrayList<>();
-		filters.add(ssoFilter(facebook(), "/login/facebook"));
-		filters.add(ssoFilter(github(), "/login/github"));
-//		filters.add(ssoFilter(twitter(), "/login/twitter"));
-		filters.add(ssoFilter(linkedin(), "/login/linkedin"));
 		filter.setFilters(filters);
-		return filter;
-	}
-
-	private Filter ssoFilter(ClientResources client, String path) {
-		OAuth2ClientAuthenticationProcessingFilter filter = new OAuth2ClientAuthenticationProcessingFilter(path);
-
-		OAuth2RestTemplate template = new OAuth2RestTemplate(client.getClient(), oauth2ClientContext);
-		filter.setRestTemplate(template);
-
-		UserInfoTokenServices tokenService = new UserInfoTokenServices(client.getResource().getUserInfoUri(), client.getClient().getClientId());
-		PrincipalExtractorBuilder extractorBuilder = new PrincipalExtractorBuilder();
-		tokenService.setPrincipalExtractor(extractorBuilder.build(client.getResource().getId()));
-
-		filter.setTokenServices(tokenService);
 		return filter;
 	}
 
@@ -120,29 +93,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		// In this way we can use it to handle redirects signaled by expceptions in authentication requests.
 		registration.setOrder(-100);
 		return registration;
-	}
-
-	@Bean
-	@ConfigurationProperties("github")
-	public ClientResources github() {
-		return new ClientResources();
-	}
-
-	@Bean
-	@ConfigurationProperties("facebook")
-	public ClientResources facebook() {
-		return new ClientResources();
-	}
-	
-//	@Bean
-//	@ConfigurationProperties("twitter")
-//	public ClientResources twitter() {
-//		return new ClientResources();
-//	}
-	
-	@Bean
-	@ConfigurationProperties("linkedin")
-	public ClientResources linkedin() {
-		return new ClientResources();
 	}
 }
