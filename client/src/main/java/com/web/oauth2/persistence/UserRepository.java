cc38a143635f063c45d0dd10e6ab7124a3b7a147
Kazuyuki DEWA <Kazuyuki.Dewa@so-net.co.jp>
2019-07-02 20:23:00 +0900

    client側で不要なファイルを除去

--- client/src/main/java/com/web/oauth2/persistence/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.web.oauth2.persistence;
-
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-	public User findByUserName(String userName);
-}
